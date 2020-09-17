fc8dc48c9d56abd10434dc170bc2e30ce4e91947
Millie Clare <Millie_Clare@outlook.com>
2020-09-17 16:30:32 +0100

    13. First tests written and passes.
    Does involve a little duplication which I'd like to change if I have time at the end

--- src/tests/items.spec.js
+++ src/tests/items.spec.js
@@ -1,2 +1,34 @@
 // tests for logic: create different set ups and ensure correct response is returned e.g. practice nurse, pass in empty object, or a string etc.
 // test that all conditions pass, not just auto pass because one matches.
+import * as shifts from "../tests/shifts.json";
+import { meetsCriteriaForShift } from "../components/items";
+import moment from "moment";
+
+moment().format();
+
+const currentDate = moment("2018-06-19T16:50:00+00:00");
+
+const user1 = {
+  id: "1234",
+  firstName: "John",
+  lastName: "Doe",
+  staffType: "gp",
+  staffTypeId: "1"
+};
+
+const user2 = {
+  id: "1235",
+  firstName: "Jane",
+  lastName: "Doe",
+  staffType: "Practice Nurse",
+  staffTypeId: "2"
+};
+
+describe("Shifts", () => {
+  it.only("returns one shift where 'staffType' === 'gp'", () => {
+    let result = shifts.data.filter(item =>
+      meetsCriteriaForShift(item, currentDate, user1)
+    );
+    expect(result).toHaveLength(6);
+  });
+});
